41d990796d7e7742688f9115c09243f6965b2989
Hunter Loftis <hunter@hunterloftis.com>
2013-04-25 13:06:15 -0400

    simple build

--- build.sh
+++
@@ -1,6 +0,0 @@
-cp demos/slide-js.html ./slide-js.html
-
-sed -i '/<!-- Content -->/{
-    s/<!-- Content -->//g
-    r demos/slide-js.html
-}' slide-js.html
--- makefile
+++ makefile
@@ -1,2 +1,4 @@
 build:
-	./build.sh
+	cat header.html > slide-js.html
+	cat demos/slide-js.html >> slide-js.html
+	cat footer.html >> slide-js.html
